1325fc1d17748e8263b1efd786146cd83f369741
Marcelo Hernandez <marcelo.hernren@hotmail.com>
2025-04-09 16:19:02 -0500

    code upload

---
+++ LU_fact/LU_fact.cpp
@@ -0,0 +1,62 @@
+#include "pch.h"
+#include <iostream>
+#include <vector>
+
+using namespace System;
+using namespace std;
+
+// Función para realizar la factorización LU
+void luDecomposition(vector<vector<double>>& A, vector<vector<double>>& L, vector<vector<double>>& U, int n) {
+    for (int i = 0; i < n; i++) {
+        // Calcular la matriz U
+        for (int k = i; k < n; k++) {
+            double sum = 0;
+            for (int j = 0; j < i; j++)
+                sum += L[i][j] * U[j][k];
+            U[i][k] = A[i][k] - sum;
+        }
+
+        // Calcular la matriz L
+        for (int k = i; k < n; k++) {
+            if (i == k)
+                L[i][i] = 1;  // Diagonal de L es 1
+            else {
+                double sum = 0;
+                for (int j = 0; j < i; j++)
+                    sum += L[k][j] * U[j][i];
+                L[k][i] = (A[k][i] - sum) / U[i][i];
+            }
+        }
+    }
+}
+
+int main() {
+    int n = 3;
+    vector<vector<double>> A = {
+        {2, -1, -2},
+        {-4, 6, 3},
+        {-4, -2, 8}
+    };
+
+    vector<vector<double>> L(n, vector<double>(n, 0));
+    vector<vector<double>> U(n, vector<double>(n, 0));
+
+    luDecomposition(A, L, U, n);
+
+    // Mostrar matrices L y U
+    cout << "Matriz L:\n";
+    for (const auto& row : L) {
+        for (double val : row)
+            cout << val << " ";
+        cout << endl;
+    }
+
+    cout << "Matriz U:\n";
+    for (const auto& row : U) {
+        for (double val : row)
+            cout << val << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
